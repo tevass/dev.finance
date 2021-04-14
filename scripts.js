3cb9d0d41bb98b413d367fc94d881f788d85c958
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2021-04-14 17:43:46 -0300

    add script.js

---
+++ scripts.js
@@ -0,0 +1,93 @@
+const Modal = {
+    open(){
+        // Abrir modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.add('active')
+    },
+    close(){
+        document
+            .querySelector('.modal-overlay')
+            .classList.remove('active')
+    }
+}
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    },
+]
+
+const Transaction = {
+    incomes() {
+        // Somar as entradas
+    },
+    expanses() {
+        // Somar as saidas
+    },
+    total() {
+        // entradas - saidas
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.inneHTMLTransaction(transaction)
+        console.log(tr)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    inneHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expanse"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover Transação">
+            </td>
+        `
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+transactions.forEach(transaction => {
+    DOM.addTransaction(transaction)
+})
